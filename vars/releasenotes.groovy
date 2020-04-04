21500aa5eaf493c90b7fea88cb6e112d56ea34dd
Muralidharan Subramanian <muralidharan.subramanian@gmail.com>
2020-04-04 13:10:05 -0400

    get Build number

--- vars/releasenotes.groovy
+++ vars/releasenotes.groovy
@@ -25,6 +25,8 @@ def date = new Date();
 def sdf = new SimpleDateFormat("MM/dd/yyyy HH.mm.ss")
 echo "Date and Time IS: " + sdf.format(date)
 
+echo "Build Number is: ${BUILD_NUMER}";
+
     if (config.changes != "false") {
         echo "changes";
     }
